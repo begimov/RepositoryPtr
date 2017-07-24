77afae0c303a692f0fc2af6e04a6e382935613e7
begimov <begimov@gmail.com>
2017-07-24 10:47:01 +0300

    basic repository, service provider to inject concrete implementation of related interface and its usage in controller added

--- app/Http/Controllers/TopicController.php
+++ app/Http/Controllers/TopicController.php
@@ -4,12 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Topic;
+use App\Repositories\Contracts\TopicRepository;
 
 class TopicController extends Controller
 {
+    protected $topics;
+
+    public function __construct(TopicRepository $topics)
+    {
+        $this->topics = $topics;
+    }
     public function index()
     {
-        $topics = Topic::get();
-        dd($topics);
+        $topics = $this->topics->all();
     }
 }
