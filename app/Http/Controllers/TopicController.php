36a04c28f1b2c371c485b9192f1c84d1caede2c2
begimov <begimov@gmail.com>
2017-07-24 10:56:30 +0300

    user repository implementation injected into topic controller

--- app/Http/Controllers/TopicController.php
+++ app/Http/Controllers/TopicController.php
@@ -5,17 +5,23 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Topic;
 use App\Repositories\Contracts\TopicRepository;
+use App\Repositories\Contracts\UserRepository;
 
 class TopicController extends Controller
 {
     protected $topics;
+    protected $users;
 
-    public function __construct(TopicRepository $topics)
+    public function __construct(TopicRepository $topics, UserRepository $users)
     {
         $this->topics = $topics;
+        $this->users = $users;
     }
     public function index()
     {
         $topics = $this->topics->all();
+        $users = $this->users->all();
+
+        dd($topics, $users);
     }
 }
